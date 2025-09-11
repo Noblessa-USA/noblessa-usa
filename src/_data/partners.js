d82a4eb12d7c3016ac0923a1c4527e2b258e31f0
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2025-09-11 11:55:48 -0400

    Added partners JS

---
+++ src/_data/partners.js
@@ -0,0 +1,176 @@
+module.exports = [
+    // Showrooms
+    {
+        name: 'Jupiter',
+        orgname: 'Noblessa',
+        address: '1405 FL A1AAlt #107, Jupiter, FL 33469',
+        city: 'Jupiter',
+        state: 'FL',
+        phone: '(561) 401-9866',
+        email: 'info@noblessa-usa.com',
+        image: '../../assets/images/showrooms/jupiteropt2.webp'
+    },
+    {
+        name: 'San Jose',
+        orgname: 'Noblessa',
+        address: '3109 Stevens Creek Blvd, San Jose, CA 95117',
+        city: 'San Jose',
+        state: 'CA',
+        phone: '(408) 674-6493',
+        email: 'info@noblessa-usa.com',
+        image: '../../assets/images/showrooms/sanjoseopt2.webp'
+    },
+    {
+        name: 'Pasadena',
+        orgname: 'Noblessa',
+        address: '474 S Arroyo Pkwy, Pasadena, CA 91105',
+        city: 'Pasadena',
+        state: 'CA',
+        phone: '(626) 314-2994',
+        email: 'info@noblessa-usa.com',
+        image: '../../assets/images/showrooms/pasadenaopt1.webp'
+    },
+    // Ambassadors
+    {
+        name: 'Cherisse Sweeney',
+        orgname: 'Basalt Interiors',
+        address: '766 Higuera Street, San Luis Obispo, CA 93401',
+        city: 'San Luis Obispo',
+        state: 'CA',
+        phone: '(805) 439-1104',
+        email: 'cherisse@basaltinteriors.com',
+        website: 'https://basaltinteriors.com/',
+        image: '/assets/images/ambassadors/cherissesweeny.png'
+    },
+    {
+        name: 'Dimitry Belov',
+        orgname: 'The Guys with Drills',
+        address: '2816 SW 2nd Ln #1N, Cape Coral, FL 33991',
+        city: 'Cape Coral',
+        state: 'FL',
+        phone: '(305) 985-8415',
+        email: 'theguyswithdrills@gmail.com',
+        website: 'https://theguyswithdrills.com',
+        image: '/assets/images/ambassadors/guyswithdrills.png'
+    },
+    {
+        name: 'Michael Memar',
+        orgname: 'Memar Interiors',
+        address: '13704 Flagstone Ln. Dallas, TX 75240',
+        city: 'Dallas',
+        state: 'TX',
+        phone: '(469) 961-6000',
+        email: 'memarinteriors@gmail.com',
+        website: 'https://memarinteriors.com',
+        image: '/assets/images/ambassadors/michaelmemar.png'
+    },
+    {
+        name: 'Stefanie Vassilicos',
+        orgname: 'Patina Interior Design',
+        address: '33 N Market St. Wailuku, HI 96793',
+        city: 'Wailuku',
+        state: 'HI',
+        phone: '(808) 633-4035',
+        email: 'martin@patinainteriordesign.com',
+        website: 'https://patinainteriordesign.com',
+        image: '/assets/images/ambassadors/patina.png'
+    },
+    {
+        name: 'Eddie Martinez',
+        orgname: 'EMDB Design',
+        address: '43836 U.S. 160 Bayfield, CO 81122',
+        city: 'Bayfield',
+        state: 'CO',
+        phone: '(970) 779-8119',
+        email: 'eddie@emdb-durango.com',
+        website: 'https://emdb-durango.com/',
+        image: '/assets/images/ambassadors/emdb.webp'
+    },
+    {
+        name: 'Tony Lou',
+        orgname: 'Advanced Construction Allies',
+        address: '2337 N Roscomare Rd Ste 8 Bel Air, CA 90077',
+        city: 'Bel Air',
+        state: 'CA',
+        phone: '(310) 623-6808',
+        email: 'info@weareaca.com',
+        website: 'https://weareaca.com/',
+        image: '/assets/images/ambassadors/aca.webp'
+    },
+    {
+        name: 'Lyndsay Roark',
+        orgname: 'VC Home and Design',
+        address: '300 E. Santa Clara St. Suite 103 Ventura, CA 93001',
+        city: 'Ventura',
+        state: 'CA',
+        phone: '(805) 232-8526',
+        email: 'iroark@vchomeanddesign.com',
+        website: 'https://vchomedesignco.com',
+        image: '/assets/images/ambassadors/vc.png'
+    },
+    {
+        name: 'Isabel Mendez',
+        orgname: 'Mendez Interior Design',
+        address: '2500 Tanglewilde St # 475, Houston, TX 77063',
+        city: 'Houston',
+        state: 'TX',
+        phone: '(832) 224-5727',
+        email: 'contact@mendezinteriordesign.com',
+        website: 'https://www.mendezinteriordesign.com',
+        image: '/assets/images/ambassadors/mendez.webp'
+    },
+    {
+        name: 'Charles Taylor',
+        orgname: 'Charles Taylor Interior Design',
+        address: '261 N. Highway 101 Suite 1152 Solana Beach, CA 92075',
+        city: 'Solana Beach',
+        state: 'CA',
+        phone: '(858) 779-4556',
+        email: 'info@cthid.com',
+        website: 'https://www.cthid.com',
+        image: '/assets/images/ambassadors/charlestaylor.webp'
+    },
+    {
+        name: 'Mindy Mai',
+        orgname: 'Dear Studio Design',
+        address: 'Newport Beach, CA 92625',
+        city: 'Newport Beach',
+        state: 'CA',
+        phone: '(949) 432-3114',
+        email: 'info@dearstudiodesign.com',
+        website: 'https://dearstudiodesign.com',
+        image: '/assets/images/ambassadors/mindymai.png'
+    },
+    //Administrators
+    {
+        name: 'Tom Rodgers',
+        orgname: 'Diginuiti',
+        city: 'Newport Beach',
+        state: 'CA',
+        phone: '(949) 432-3114',
+        email: 'tom@diginuiti.com',
+        website: 'https://diginuiti.com',
+        image: '/assets/images/ambassadors/tomrodgers.png'
+    },
+    {
+        name: 'Zade Dohan',
+        orgname: 'Callowhill Web Designs LLC',
+        city: 'Philadelphia',
+        state: 'PA',
+        phone: '(317) 793-7388',
+        email: 'zadohan@callowhillwebdesigns.com',
+        website: 'https://callowhillwebdesigns.com',
+        image: '/assets/images/ambassadors/zadedohan.png'
+    },
+    {
+        name: 'Andrea',
+        orgname: 'Noblessa USA',
+        city: 'Pasadena',
+        state: 'CA',
+        phone: '(123) 456-7890',
+        email: 'info@noblessa-usa.com',
+        website: 'https://noblessa-usa.com',
+        image: '/assets/images/ambassadors/andrea.png'
+    },
+
+]
